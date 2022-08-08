1738de105c39deb5ad964026cfb37144cef66d29
yumengfei <mengfei_yu@loongjoy.com>
2022-08-08 18:09:47 +0800

    sqlx

--- sqlx/main.go
+++ sqlx/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql/driver"
 	"errors"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
 )
 
 var db *sqlx.DB
@@ -29,7 +31,7 @@ func main() {
 		fmt.Printf("connect DB failed, err:%v\n", err)
 		return
 	}
-
+	defer db.Close()
 	//fmt.Println("单条查询")
 	//queryRowDemo()
 	//fmt.Println("多条查询")
@@ -47,9 +49,35 @@ func main() {
 	//fmt.Println("预处理")
 	//nameQuery()
 
-	fmt.Println("事务")
-	err = transactionDemo()
-	fmt.Println(err)
+	//fmt.Println("事务")
+	//err = transactionDemo()
+	//fmt.Println(err)
+
+	u1 := Users{Name: "七米", Password: "123456", Address: "123456", Phone: "1333333333", Money: 20}
+	u2 := Users{Name: "q1mi", Password: "123456", Address: "123456", Phone: "1333333333", Money: 20}
+	u3 := Users{Name: "小王子", Password: "123456", Address: "123456", Phone: "1333333333", Money: 20}
+
+	// 方法1
+	//users := []*Users{&u1, &u2, &u3}
+	//err = BatchInsertUsers(users)
+	//if err != nil {
+	//	fmt.Printf("BatchInsertUsers failed, err:%v\n", err)
+	//}
+
+	// 方法2
+	//fmt.Println("BatchInsertUsers2")
+	//users2 := []interface{}{u1, u2, u3}
+	//err = BatchInsertUsers2(users2)
+	//if err != nil {
+	//	fmt.Printf("BatchInsertUsers2 failed, err:%v\n", err)
+	//}
+
+	// 方法3
+	users3 := []*Users{&u1, &u2, &u3}
+	err = BatchInsertUsers3(users3)
+	if err != nil {
+		fmt.Printf("BatchInsertUsers3 failed, err:%v\n", err)
+	}
 }
 
 
@@ -229,4 +257,99 @@ func transactionDemo() (err error){
 		return errors.New("exec sqlStr1 failed")
 	}
 	return err
+}
+
+// sqlx.In的批量插入示例
+
+type Users struct {
+	Name     string `db:"name"`
+	Password string `db:"password"`
+	Address  string `db:"address"`
+	Phone    string `db:"phone"`
+	Money    int `db:"money"`
+}
+
+// 自己拼接语句实现批量插入
+func BatchInsertUsers(users []*Users) error {
+	// 存放 (?, ?) 的slice
+	valueStrings := make([]string, 0, len(users))
+
+	// 存放values的slice
+	valueArgs := make([]interface{}, 0, len(users) * 2)
+
+	// 遍历users准备相关数据
+	for _, u := range users {
+		// 此处占位符要与插入值的个数对应
+		valueStrings = append(valueStrings, "(?, ?, ?, ?, ?)")
+		valueArgs = append(valueArgs, u.Name)
+		valueArgs = append(valueArgs, u.Password)
+		valueArgs = append(valueArgs, u.Address)
+		valueArgs = append(valueArgs, u.Phone)
+		valueArgs = append(valueArgs, u.Money)
+	}
+
+	// 自行拼接要执行的具体语句
+	stmt := fmt.Sprintf("INSERT INTO user (name,password,address,phone,money) VALUES %s",
+		strings.Join(valueStrings, ","))
+	_, err := db.Exec(stmt, valueArgs...)
+	return err
+}
+
+
+// 使用sqlx.In实现批量插入
+func (u Users) Value() (driver.Value, error) {
+	return []interface{}{u.Name, u.Password, u.Address, u.Phone, u.Money}, nil
+}
+
+// BatchInsertUsers2 使用sqlx.In帮我们拼接语句和参数, 注意传入的参数是[]interface{}
+func BatchInsertUsers2(users []interface{}) error {
+	fmt.Println("users: ", users)
+	query, args, _ := sqlx.In(
+		"INSERT INTO user (name,password,address,phone,money) VALUES (?), (?), (?)",
+		users..., // 如果arg实现了 driver.Valuer, sqlx.In 会通过调用 Value()来展开它
+	)
+	fmt.Println(query) // 查看生成的querystring
+	fmt.Println(args)  // 查看生成的args
+	_, err := db.Exec(query, args...)
+	return err
+}
+
+// BatchInsertUsers3 使用NamedExec实现批量插入
+func BatchInsertUsers3(users []*Users) error {
+	_, err := db.NamedExec("INSERT INTO user (name,password,address,phone,money) VALUES (:name,:password,:address,:phone,:money)", users)
+	return err
+}
+
+// sqlx.In的查询示例
+// QueryByIDs 根据给定ID查询
+func QueryByIDs(ids []int)(users []Users, err error){
+	// 动态填充id
+	query, args, err := sqlx.In("SELECT name FROM user WHERE id IN (?)", ids)
+	if err != nil {
+		return
+	}
+	// sqlx.In 返回带 `?` bindvar的查询语句, 我们使用Rebind()重新绑定它
+	query = db.Rebind(query)
+
+	err = db.Select(&users, query, args...)
+	return
+}
+
+// QueryAndOrderByIDs 按照指定id查询并维护顺序
+func QueryAndOrderByIDs(ids []int)(users []Users, err error){
+	// 动态填充id
+	strIDs := make([]string, 0, len(ids))
+	for _, id := range ids {
+		strIDs = append(strIDs, fmt.Sprintf("%d", id))
+	}
+	query, args, err := sqlx.In("SELECT name, age FROM user WHERE id IN (?) ORDER BY FIND_IN_SET(id, ?)", ids, strings.Join(strIDs, ","))
+	if err != nil {
+		return
+	}
+
+	// sqlx.In 返回带 `?` bindvar的查询语句, 我们使用Rebind()重新绑定它
+	query = db.Rebind(query)
+
+	err = db.Select(&users, query, args...)
+	return
 }
